dfb6e161349c84376ade24fc02c416ee30d13145
Cucu Toma Daniel <61708051+DanielTomaCucu@users.noreply.github.com>
2023-08-23 20:44:41 +0300

    add menu and routing to all pages (#3)
    Co-authored-by: Cucu Toma Daniel <cucutomadaniel@192-168-0-239.rdsnet.ro>

--- MultiversX_Explorer/src/app/app-routing.module.ts
+++ MultiversX_Explorer/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountsComponent } from './accounts/accounts.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { BlocksComponent } from './blocks/blocks.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NftsComponent } from './nfts/nfts.component';
+import { TokensComponent } from './tokens/tokens.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { ValidatorsComponent } from './validators/validators.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'blocks', component: BlocksComponent },
+  { path: 'transactions', component: TransactionsComponent },
+  { path: 'accounts', component: AccountsComponent },
+  { path: 'tokens', component: TokensComponent },
+  { path: 'nfts', component: NftsComponent },
+  { path: 'validators', component: ValidatorsComponent },
+  { path: 'analytics', component: AnalyticsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- MultiversX_Explorer/src/app/app.component.ts
+++ MultiversX_Explorer/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'MultiversX_Explorer';
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
+  showFiller = false;
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
