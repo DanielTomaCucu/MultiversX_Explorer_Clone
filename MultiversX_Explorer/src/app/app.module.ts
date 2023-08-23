1d61ae35727cf9aaa7cfc9d6674975430f7e2181
Cucu Toma Daniel <cucutomadaniel@192-168-0-239.rdsnet.ro>
2023-08-23 20:43:39 +0300

    add menu and routing to all pages

--- MultiversX_Explorer/src/app/app.module.ts
+++ MultiversX_Explorer/src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatListModule } from '@angular/material/list';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { LayoutModule } from '@angular/cdk/layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccountsComponent } from './accounts/accounts.component';
+import { TokensComponent } from './tokens/tokens.component';
+import { NftsComponent } from './nfts/nfts.component';
+import { ValidatorsComponent } from './validators/validators.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { BlocksComponent } from './blocks/blocks.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 @NgModule({
-  declarations: [AppComponent, DashboardComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MatCardModule],
+  declarations: [AppComponent, DashboardComponent, AccountsComponent, TokensComponent, NftsComponent, ValidatorsComponent, AnalyticsComponent, BlocksComponent, TransactionsComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatCardModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    LayoutModule,
+    MatListModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
