3834d6f1fbb7188d947e6ec1e5d9ae71a13bc5ad
Cucu Toma Daniel <cucutomadaniel@192-168-0-239.rdsnet.ro>
2023-08-31 09:44:10 +0300

    add some info for providers-details page

--- MultiversX_Explorer/src/app/validators/providers/providers-details/providers-details.component.ts
+++ MultiversX_Explorer/src/app/validators/providers/providers-details/providers-details.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { LoadingSpinnerService } from 'src/app/shared/loading-spinner.service';
 import { ProvidersDetailsService } from './providers-details.service';
 
 @Component({
@@ -10,10 +11,14 @@ import { ProvidersDetailsService } from './providers-details.service';
 })
 export class ProvidersDetailsComponent {
   providerDetails: any;
+  providerDetailsSummar: any;
   subscription: Subscription;
+  isFeatured = localStorage.getItem('featured');
+  isLoading$ = this.loadingSpinnerService.isLoading.asObservable();
   constructor(
     private providerDetailsService: ProvidersDetailsService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private loadingSpinnerService: LoadingSpinnerService
   ) {
     this.subscription = new Subscription();
   }
@@ -23,9 +28,13 @@ export class ProvidersDetailsComponent {
       this.providerDetailsService
         .getProvidersDetails(addr)
         .subscribe((data) => {
-          console.log(data);
+          this.providerDetails = data;
         })
     );
+    const storedDetails = localStorage.getItem('providerDetails');
+    if (storedDetails) {
+      this.providerDetailsSummar = JSON.parse(storedDetails);
+    }
   }
 
   ngOnDelete() {
