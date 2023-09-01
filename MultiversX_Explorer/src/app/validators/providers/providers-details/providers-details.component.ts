ab935fca44d3fa90275ebc35a1675b27f6097256
Cucu Toma Daniel <61708051+DanielTomaCucu@users.noreply.github.com>
2023-09-01 17:55:01 +0300

    Validators (#26)
    * add providers page
    
    * add some info for providers-details page
    
    * add table to providers details
    
    ---------
    
    Co-authored-by: Cucu Toma Daniel <cucutomadaniel@192-168-0-239.rdsnet.ro>

--- MultiversX_Explorer/src/app/validators/providers/providers-details/providers-details.component.ts
+++ MultiversX_Explorer/src/app/validators/providers/providers-details/providers-details.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { LoadingSpinnerService } from 'src/app/shared/loading-spinner.service';
 import { ProvidersDetailsService } from './providers-details.service';
@@ -12,16 +14,33 @@ import { ProvidersDetailsService } from './providers-details.service';
 export class ProvidersDetailsComponent {
   providerDetails: any;
   providerDetailsSummar: any;
+  providerNodes: any;
   subscription: Subscription;
   isFeatured = localStorage.getItem('featured');
   isLoading$ = this.loadingSpinnerService.isLoading.asObservable();
+  dataSource!: MatTableDataSource<any>;
+  currentFrom: number = 0;
+  itemsSize: number = 25;
   constructor(
     private providerDetailsService: ProvidersDetailsService,
     private route: ActivatedRoute,
-    private loadingSpinnerService: LoadingSpinnerService
+    private loadingSpinnerService: LoadingSpinnerService,
+    private router:Router
   ) {
     this.subscription = new Subscription();
   }
+
+  displayedColumns: string[] = [
+    'public_key',
+    'name',
+    'shard',
+    'version',
+    'ignored_signatures',
+    'status',
+    'rating',
+    'nonce',
+  ];
+  @ViewChild(MatSort) sort!: MatSort;
   ngOnInit() {
     const addr: any = this.route.snapshot.paramMap.get('address');
     this.subscription.add(
@@ -35,8 +54,47 @@ export class ProvidersDetailsComponent {
     if (storedDetails) {
       this.providerDetailsSummar = JSON.parse(storedDetails);
     }
+    this.subscription.add(
+      this.route.queryParams.subscribe((params) => {
+        const page = +params['page'] || 1;
+        this.currentFrom = (page - 1) * this.itemsSize;
+        this.loadNodes();
+      })
+    );
   }
 
+  loadNodes() {
+    const addr: any = this.route.snapshot.paramMap.get('address');
+    this.subscription.add(
+      this.providerDetailsService
+        .getProvidersNodes(addr, this.currentFrom, this.itemsSize)
+        .subscribe((data) => {
+          console.log(data);
+          this.dataSource = new MatTableDataSource(data);
+          this.dataSource.sort = this.sort;
+        })
+    );
+  }
+  nextPage() {
+    const nextPage = this.currentFrom / this.itemsSize + 2;
+    this.updatePage(nextPage);
+  }
+
+  prevPage() {
+    const prevPage = this.currentFrom / this.itemsSize;
+    if (prevPage > 0) {
+      this.updatePage(prevPage);
+    }
+  }
+  updatePage(page: number) {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { page: page },
+      queryParamsHandling: 'merge',
+    });
+  }
+
+  redirectToNode(bls: string) {}
   ngOnDelete() {
     this.subscription.unsubscribe();
   }
--- MultiversX_Explorer/src/app/validators/providers/providers-details/providers-details.service.ts
+++ MultiversX_Explorer/src/app/validators/providers/providers-details/providers-details.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { environment } from 'src/enviroments/enviroment';
 @Injectable({
   providedIn: 'root',
@@ -9,10 +9,16 @@ export class ProvidersDetailsService {
 
 
   private providerDeails = `${environment.baseUrl}/providers`;
-  constructor(private http: HttpClient) {}
+  private providerNodes = `${environment.baseUrl}/nodes`;
+
+  constructor(private http: HttpClient) { }
+
   getProvidersDetails(address: string): Observable<any> {
     return this.http.get<any>(`${this.providerDeails}/${address}`);
   }
-
-
+  getProvidersNodes(provider:string, from:number, size:number): Observable<any>{
+    return this.http.get<any>(
+      `${this.providerNodes}?from=${from}&size=${size}&provider=${provider}`
+    );
+  }
 }
